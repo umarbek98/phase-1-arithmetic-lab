fcf9c63c3103bb0c634238d3096a15fbcd1a5bb1
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-01-09 22:56:31 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+let num3 = 32;
+let num4 = 7;
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
